53a9baa3059612a7fa99d6268bb82a0c5fa56552
DKNathan7 <emilioharo2@gmail.com>
2026-04-10 03:33:32 +0200

    Cambios en el header

--- E-Comerce_Cv/src/App.jsx
+++ E-Comerce_Cv/src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css"
+import logo from "/Img/LogoE-comerce.png"
+import { FiSearch, FiMenu } from "react-icons/fi"
 
 export function App () {
     return (
-        <div>
-            <header className="bg-amber-200">
-                <div>
-                    <a href="#"><img src="./Img/LogoE-comerce.png" alt="logo_EComerce" /></a>
-                    opc.patines
-                    <input type="text" />lupa
+        <div className="min-h-screen bg-gray-700 text-white">
+            <header className="">
+                <div className="flex items-center justify-center gap-6">
+                    <a href="#">
+                        <img src={logo} alt="logo_EComerce" className="w-15"/>
+                    </a>
+                    <div><FiMenu className=""/><h3>Todas las categorías</h3></div>
+                    <div className="relative">
+                        <input type="text" className="border rounded pl-8 flex-1" placeholder="Buscar" name="search"/>
+                        <FiSearch className="absolute top-1/2 left-2 -translate-y-1/2"/>
+                    </div>
+                    
                     <span>Mi cuenta</span>
                     <span>Mi cesta</span>
                     </div>
--- E-Comerce_Cv/src/main.jsx
+++ E-Comerce_Cv/src/main.jsx
@@ -1,4 +1,4 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx"
 const root = createRoot(document.getElementById('app'))
-root.render(<App />)
+root.render(<App />) 
